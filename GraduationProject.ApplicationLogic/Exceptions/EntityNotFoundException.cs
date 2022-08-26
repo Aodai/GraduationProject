3ec6540e363b49c9e1c3f868f9ecc1b36c3dccfc
Aodai <odai@odaiq.co>
2022-08-26 21:03:08 +0300

    Create EntityNotFoundException.cs

---
+++ GraduationProject.ApplicationLogic/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace GraduationProject.ApplicationLogic.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public Guid EntityID { get; private set; }
+        public EntityNotFoundException(Guid id) : base($"Entity with id {id} was not found") { }
+        public EntityNotFoundException() : base() { }
+        public EntityNotFoundException(string message) : base(message) { }
+        public EntityNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
